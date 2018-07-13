80f5ad31be78b49ac4bf836a28e1f438b8126c41
MarcosMazzei <mazzeimarcosv@gmail.com>
2018-07-13 18:37:08 -0300

    Parte do migo (precisa terminar os calculos)

--- Desktop/ProjetoEconomia/src/pacoteecono/PaginaInicial.java
+++ Desktop/ProjetoEconomia/src/pacoteecono/PaginaInicial.java
@@ -42,11 +42,15 @@ public class PaginaInicial extends javax.swing.JFrame {
         btn_ElastP = new javax.swing.JButton();
         jLabel8 = new javax.swing.JLabel();
         btn_ElastR = new javax.swing.JButton();
+        jLabel10 = new javax.swing.JLabel();
+        btn_pib = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Cambria", 1, 24)); // NOI18N
         jLabel1.setText("Software de Economia");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 484, 26));
 
         btn_cambio.setText("Entrar");
         btn_cambio.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -59,12 +63,15 @@ public class PaginaInicial extends javax.swing.JFrame {
                 btn_cambioActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_cambio, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, -1, -1));
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel2.setText("Câmbio e Investimentos");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, -1, 21));
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel3.setText("Custo, Receita e Lucro");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, -1, 20));
 
         btn_lucro.setText("Entrar");
         btn_lucro.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -77,9 +84,11 @@ public class PaginaInicial extends javax.swing.JFrame {
                 btn_lucroActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_lucro, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, -1, -1));
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel4.setText("Demanda");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 240, 60, 21));
 
         btn_demanda.setText("Entrar");
         btn_demanda.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -92,9 +101,11 @@ public class PaginaInicial extends javax.swing.JFrame {
                 btn_demandaActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_demanda, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, -1, -1));
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel5.setText("Elasticidade");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 60, -1, 21));
 
         btn_Elast.setText("Entrar");
         btn_Elast.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -107,9 +118,11 @@ public class PaginaInicial extends javax.swing.JFrame {
                 btn_ElastActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_Elast, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 80, -1, -1));
 
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel6.setText("Elasticidade Cruzada");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 120, -1, 21));
 
         btn_ElastC.setText("Entrar");
         btn_ElastC.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -122,9 +135,11 @@ public class PaginaInicial extends javax.swing.JFrame {
                 btn_ElastCActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_ElastC, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 140, -1, -1));
 
         jLabel7.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel7.setText("Elasticidade Preço");
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 180, -1, 21));
 
         btn_ElastP.setText("Entrar");
         btn_ElastP.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -137,9 +152,11 @@ public class PaginaInicial extends javax.swing.JFrame {
                 btn_ElastPActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_ElastP, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 200, -1, -1));
 
         jLabel8.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel8.setText("Elasticidade Renda");
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 240, -1, 21));
 
         btn_ElastR.setText("Entrar");
         btn_ElastR.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -152,90 +169,24 @@ public class PaginaInicial extends javax.swing.JFrame {
                 btn_ElastRActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_ElastR, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 260, -1, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel2))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(35, 35, 35)
-                        .addComponent(btn_cambio)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 91, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel6)
-                    .addComponent(jLabel4)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(2, 2, 2)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel8)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(btn_ElastP)
-                                    .addGap(190, 190, 190))
-                                .addComponent(jLabel7, javax.swing.GroupLayout.Alignment.LEADING))
-                            .addComponent(btn_ElastR)))
-                    .addComponent(btn_ElastC)
-                    .addComponent(btn_Elast)
-                    .addComponent(btn_demanda)))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel3))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(33, 33, 33)
-                        .addComponent(btn_lucro)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(btn_cambio)
-                            .addComponent(btn_demanda)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(29, 29, 29)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btn_lucro))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btn_Elast)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btn_ElastC)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btn_ElastP)
-                .addGap(16, 16, 16)
-                .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btn_ElastR)
-                .addContainerGap(39, Short.MAX_VALUE))
-        );
+        jLabel10.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel10.setText("PIB");
+        getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, -1, -1));
+
+        btn_pib.setText("Entrar");
+        btn_pib.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_pibMouseClicked(evt);
+            }
+        });
+        btn_pib.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_pibActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_pib, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 200, -1, -1));
 
         pack();
         setLocationRelativeTo(null);
@@ -317,6 +268,16 @@ public class PaginaInicial extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_btn_ElastRActionPerformed
 
+    private void btn_pibMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_pibMouseClicked
+        Frm_PIB frm_PIB = new Frm_PIB();
+         frm_PIB.setVisible(true);
+         this.dispose();
+    }//GEN-LAST:event_btn_pibMouseClicked
+
+    private void btn_pibActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_pibActionPerformed
+                // TODO add your handling code here:
+    }//GEN-LAST:event_btn_pibActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -360,7 +321,9 @@ public class PaginaInicial extends javax.swing.JFrame {
     private javax.swing.JButton btn_cambio;
     private javax.swing.JButton btn_demanda;
     private javax.swing.JButton btn_lucro;
+    private javax.swing.JButton btn_pib;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
